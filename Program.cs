72b69ae4fde5db25b67819716398651dfb14481a
twinbird827 <twinbird827@gmail.com>
2018-12-20 17:53:03 +0900

    正規表現の確認

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// 正規表現の確認
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "{AAA}{BBB}";
+
+            var r1 = Regex.Matches(s1, @"(?<={)[^}]+");
+            
+            foreach (Match s in r1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
